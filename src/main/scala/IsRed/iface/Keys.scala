1d04fdac10098e11addabdc891d20f17e820bb44
Nycto <James@RoundEights.com>
2013-02-17 21:08:09 -0800

    Add Keys interface definition

---
+++ src/main/scala/IsRed/iface/Keys.scala
@@ -0,0 +1,78 @@
+package com.roundeights.isred
+
+/**
+ * Methods for interacting with Redis Keys
+ */
+trait Keys {
+
+    /** A success or failures result */
+    type AckResult
+
+    /** A string result */
+    type StringResult
+
+    /** A list of keys */
+    type KeyListResult
+
+    /** An integer result */
+    type IntResult
+
+    /** A single key result */
+    type KeyResult
+
+    /** A result describing the type of a key */
+    type TypeResult
+
+    /** Delete a key */
+    def del ( key: Key, keys: Key* ): AckResult
+
+    /** Return a serialized version of the value stored at the specified key. */
+    def dump ( key: Key ): StringResult
+
+    /** Determine if a key exists */
+    def exists ( key: Key ): AckResult
+
+    /** Set a key's time to live in seconds */
+    def expire ( key: Key, seconds: Int ): AckResult
+
+    /** Set the expiration for a key as a UNIX timestamp */
+    def expireAt ( key: Key, timestamp: Int ): AckResult
+
+    /** Find all keys matching the given pattern */
+    def keys ( pattern: String ): KeyListResult
+
+    /** Remove the expiration from a key */
+    def persist ( key: Key ): AckResult
+
+    /** Set a key's time to live in milliseconds */
+    def pExpire ( key: Key, milliseconds: Int ): AckResult
+
+    /** Set the expiration for a key as a UNIX timestamp specified in milliseconds */
+    def pExpireAt ( key: Key, milliTimestamp: Int ): AckResult
+
+    /** Get the time to live for a key in milliseconds */
+    def pTTL ( key: Key ): IntResult
+
+    /** Return a random key from the keyspace */
+    def randomKey (): KeyResult
+
+    /** Rename a key */
+    def rename ( key: Key, newkey: Key ): AckResult
+
+    /** Rename a key, only if the new key does not exist */
+    def renameNX ( key: Key, newkey: Key ): AckResult
+
+    /**
+     * Create a key using the provided serialized value, previously
+     * obtained using DUMP.
+     */
+    def restore ( key: Key, ttl: Int, serialized: String ): AckResult
+
+    /** Get the time to live for a key */
+    def ttl ( key: Key ): IntResult
+
+    /** Determine the type stored at key */
+    def keyType ( key: Key ): TypeResult
+
+}
+
