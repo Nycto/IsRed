0c51ae2cf44b184d29b4d386db7bc0540ff93e8e
Nycto <James@RoundEights.com>
2013-02-24 21:04:11 -0800

    Add get methods to iface

--- src/main/scala/IsRed/Iface.scala
+++ src/main/scala/IsRed/Iface.scala
@@ -51,5 +51,52 @@ trait Iface {
     /** A result describing the type of a key */
     type KeyTypeResult
 
+
+    /** A success or failure result */
+    def getAck( command: Command ): AckResult
+
+
+    /** A string, int or float result */
+    def getBulk( command: Command ): BulkResult
+
+    /** An integer result */
+    def getInt( command: Command ): IntResult
+
+    /** A floating point number result */
+    def getFloat( command: Command ): FloatResult
+
+    /** A Boolean result */
+    def getBool( command: Command ): BoolResult
+
+
+    /** An optional bulk result */
+    def getOptBulk( command: Command ): OptBulkResult
+
+    /** A set of bulk values */
+    def getBulkSet( command: Command ): BulkSetResult
+
+    /** A sequence of bulk values */
+    def getBulkSeq( command: Command ): BulkSeqResult
+
+    /** A map of bulk values */
+    def getBulkMap( command: Command ): BulkMapResult
+
+
+    /**
+     * The result of a pop. This is generally a tuple where the first
+     * element is the list that was popped from, and the second element is
+     * the value that was popped.
+     */
+    def getPop( command: Command ): PopResult
+
+    /** A list of keys */
+    def getKeyList( command: Command ): KeyListResult
+
+    /** A single key result */
+    def getKey( command: Command ): KeyResult
+
+    /** A result describing the type of a key */
+    def getKeyType( command: Command ): KeyTypeResult
+
 }
 
